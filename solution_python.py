6d3e2d525e7563376b737daea064f9cbe0c2d0f4
Hashem <hashemalnader@gmail.com>
2020-07-10 15:00:32 -0400

    submission

--- solution_python.py
+++ solution_python.py
@@ -1,23 +1,62 @@
+class Node:
+    def __init__(self, data):
+        self.prevN = None
+        self.nextN = None
+        self.data = data
+class dLinkList:
+    def __init__(self):
+        self.first = None
+        self.last = None
+        self.len = 0
+    def appendNode(self, val):
+        node = Node(val)
+        node.prevN = self.last
+        self.last = node
+        if self.first == None:
+            self.first = node
+        self.len += 1
+        
 class EventSourcer():
     # Do not change the signature of any functions
 
     def __init__(self):
         self.value = 0
+        self.history = dLinkList()
 
     def add(self, num: int):
+        self.history.appendNode(self.value)
+        self.value += num
         pass
 
     def subtract(self, num: int):
+        self.history.appendNode(self.value)
+        self.value -= num
         pass
 
     def undo(self):
+        self.history.appendNode(self.value)
+        if self.history.last == None or self.history.last.prevN == None:
+            print('Nothing to undo')
+        else:
+            self.value = self.history.last.prevN.data
         pass
 
     def redo(self):
+        if self.history.last == None:
+            print('Nothing to redo')
+        else:
+            self.value = self.history.last.data
         pass
 
     def bulk_undo(self, steps: int):
+        for i in range(0, steps):
+            self.undo
         pass
 
     def bulk_redo(self, steps: int):
+        for i in range(0, steps):
+            self.redo
         pass
+
+
+    
